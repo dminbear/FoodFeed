2a21975c65c39eb9ff6ece34c143fe133d223102
dminbear <bearbuttconfessions@gmail.com>
2016-03-04 19:33:07 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    database: {
+        url: "ds053668.mlab.com:53668",
+        name: "team15bizhacks",
+        username: "team15Bizhacks",
+        password: "team15isawesome"
+    }
+};
+module.exports = config;
+//# sourceMappingURL=config.js.map
---
+++ config.ts
@@ -0,0 +1,11 @@
+var config = {
+    database:{
+    url: "ds053668.mlab.com:53668",
+    name: "team15bizhacks",
+    username: "team15Bizhacks",
+    password: "team15isawesome"
+    }
+
+}
+
+export = config;
